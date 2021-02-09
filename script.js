abd6914658fc050298b561c5b9ff5553ad14b049
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-09 18:43:02 +0100

    show popup when form contains no error.

--- script.js
+++ script.js
@@ -58,6 +58,21 @@ const checkMail = (email) => {
     }
 };
 
+const checkErrors = () => {
+    const allInputs = document.querySelectorAll(".form-box");
+    let errorCount = 0;
+
+    allInputs.forEach((el) => {
+        if (el.classList.contains("error")) {
+            errorCount++;
+        }
+    });
+
+    if (errorCount === 0) {
+        popup.classList.add("show-popup");
+    }
+};
+
 sendBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -66,6 +81,7 @@ sendBtn.addEventListener("click", (e) => {
     checkLength(pass, 8);
     checkPassword(pass, pass2);
     checkMail(email);
+    checkErrors();
 });
 
 clearBtn.addEventListener("click", (e) => {
@@ -73,5 +89,6 @@ clearBtn.addEventListener("click", (e) => {
 
     [username, pass, pass2, email].forEach((el) => {
         el.value = "";
+        clearError(el);
     });
 });
