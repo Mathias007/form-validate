22f729a815aed80dc642b265485142e8441ce62d
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-09 18:36:05 +0100

    Validate email using regex and test function.

--- script.js
+++ script.js
@@ -48,6 +48,16 @@ const checkPassword = (pass1, pass2) => {
     }
 };
 
+const checkMail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (re.test(email.value)) {
+        clearError(email);
+    } else {
+        showError(email, "Adres e-mail jest niepoprawny.");
+    }
+};
+
 sendBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -55,6 +65,7 @@ sendBtn.addEventListener("click", (e) => {
     checkLength(username, 3);
     checkLength(pass, 8);
     checkPassword(pass, pass2);
+    checkMail(email);
 });
 
 clearBtn.addEventListener("click", (e) => {
