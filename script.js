8f9ec4a6ce95ea7d1ad4e3c05608278a287ccdd3
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-09 18:27:44 +0100

    add checkLength and checkPassword functions.

--- script.js
+++ script.js
@@ -30,10 +30,31 @@ const checkForm = (input) => {
     });
 };
 
+const checkLength = (input, min) => {
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${input.previousElementSibling.innerText.slice(
+                0,
+                -1
+            )} składa się z min. ${min} znaków.`
+        );
+    }
+};
+
+const checkPassword = (pass1, pass2) => {
+    if (pass1.value !== pass2.value) {
+        showError(pass2, "Hasła różnią się od siebie.");
+    }
+};
+
 sendBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     checkForm([username, pass, pass2, email]);
+    checkLength(username, 3);
+    checkLength(pass, 8);
+    checkPassword(pass, pass2);
 });
 
 clearBtn.addEventListener("click", (e) => {
